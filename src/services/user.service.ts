99bedecb5dc456a92c0b625e5395fd3def147ae5
tactical-testicle <escom.gustavo7@gmail.com>
2025-11-24 17:23:11 -0600

    users salt

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,78 +1,81 @@
 import { UserDAO } from '../daos/user.dao';
 import IUser from '../interfaces/user.interface';
 import logger from '../../lib/logger';
+import EncryptioClass from "../utils/encryption.util";
 
 export default class UserService {
-    
+    static encryptClass = new EncryptioClass();
+
     static async createUser(body: IUser) {
-    try {
-      const exists = await UserDAO.getUserByFicha(body.ficha);
-      console.log("exists: ", exists)
-      if (exists) {
-        return {
-          ok: false,
-          message: `La ficha ${body.ficha} ya está registrada.`,
-          code: 409
-        };
-      }
-      const newRole = await UserDAO.create(body);
-      return {
-        ok: true,
-        message: 'Creado correctamente',
-        response: newRole,
-        code: 201
-      };
-    } catch (error) {
-      logger.error(`[service/user/create]: ${error}`);
-      return {
-        ok: false,
-        message: 'Error interno al crear',
-        code: 500
-      };
-    }
-  }
-
-
-    async getUserByFicha(ficha: number){
-        try{
-            const existUser = await UserDAO.getUserByFicha(ficha)
-            return existUser
-        }catch(err){
-            
-            throw err
-        }finally{
+        try {
+            const exists = await UserDAO.getUserByFicha(body.ficha);
+            console.log("exists: ", exists)
+            if (exists) {
+                return {
+                    ok: false,
+                    message: `La ficha ${body.ficha} ya está registrada.`,
+                    code: 409
+                };
+            }
+            body.salt = await this.encryptClass.hashPassword(body.password);
+            const newRole = await UserDAO.create(body);
+            return {
+                ok: true,
+                message: 'Creado correctamente',
+                response: newRole,
+                code: 201
+            };
+        } catch (error) {
+            logger.error(`[service/user/create]: ${error}`);
+            return {
+                ok: false,
+                message: 'Error interno al crear',
+                code: 500
+            };
         }
     }
 
-    async getUserById(id: string){
-        try{
+
+    async getUserByFicha(ficha: number) {
+        try {
+            const existUser = await UserDAO.getUserByFicha(ficha)
+            return existUser
+        } catch (err) {
+
+            throw err
+        } finally {
+        }
+    }
+
+    async getUserById(id: string) {
+        try {
             const existUser = await UserDAO.getUserById(id)
             return existUser
-        }catch(err){
-            
+        } catch (err) {
+
             throw err
-        }finally{
+        } finally {
         }
     }
 
     static async getUsers() {
-    try {
-      const puestos = await UserDAO.findAll();
-      return {
-        ok: true,
-        message: 'Lista obtenida',
-        response: puestos,
-        code: 200
-      };
-    } catch (error) {
-      logger.error(`[service/user/list]: ${error}`);
-      return {
-        ok: false,
-        message: 'Error al obtener usuarios',
-        code: 500
-      };
+        try {
+            const puestos = await UserDAO.findAll();
+            return {
+                ok: true,
+                message: 'Lista obtenida',
+                response: puestos,
+                code: 200
+            };
+        } catch (error) {
+            logger.error(`[service/user/list]: ${error}`);
+            return {
+                ok: false,
+                message: 'Error al obtener usuarios',
+                code: 500
+            };
+        }
     }
-  }
-    
+
 
 }
