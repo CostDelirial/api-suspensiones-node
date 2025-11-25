9aea16f4447807928a74d5ed6fdad8639d0ae715
tactical-testicle <escom.gustavo7@gmail.com>
2025-11-24 18:08:51 -0600

    users salt y pasword

--- src/utils/encryption.util.ts
+++ src/utils/encryption.util.ts
@@ -1,37 +1,45 @@
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
-import  IUser  from '../interfaces/user.interface'
+import IUser from '../interfaces/user.interface'
 
 
 
 export default class EncryptioClass {
 
-    private  secret = process.env.JWT_SECRET || 'defaultSecret'
+    private secret = process.env.JWT_SECRET || 'defaultSecret'
     //private secret = 'a-string-secret-at-least-256-bits-long'
 
 
-    generateToken(user: any){
-        console.log("1.- SECRET: "+this.secret)
-        return jwt.sign({ user }, this.secret, { expiresIn: '1h', algorithm: "HS512"})
+    generateToken(user: any) {
+        console.log("1.- SECRET: " + this.secret)
+        return jwt.sign({ user }, this.secret, { expiresIn: '1h', algorithm: "HS512" })
     }
 
-    verifyToken(token: string):any{
-        try{
+    verifyToken(token: string): any {
+        try {
             return jwt.verify(token, String(this.secret))
-        }catch(error){
+        } catch (error) {
             throw new Error('this token is not valid')
         }
     }
 
-    async hashPassword(password: string):Promise<string>{
-            const saltRounds = 10
-            const salt = await bcrypt.genSalt(saltRounds)
-            return bcrypt.hash(password,salt)
+    async generateSalt(): Promise<string> {
+        return bcrypt.genSalt(10);
     }
 
-    async verifyPassword(plainPassword: string, hashPassword: string):Promise<boolean>{
+    async hashPasswordWithSalt(password: string, salt: string): Promise<string> {
+        return bcrypt.hash(password, salt);
+    }
+
+    async hashPassword(password: string): Promise<string> {
+        const saltRounds = 10
+        const salt = await bcrypt.genSalt(saltRounds)
+        return bcrypt.hash(password, salt)
+    }
+
+    async verifyPassword(plainPassword: string, hashPassword: string): Promise<boolean> {
         console.log(plainPassword + " vs " + hashPassword)
-        return bcrypt.compare(plainPassword,hashPassword)
+        return bcrypt.compare(plainPassword, hashPassword)
     }
 
 }
