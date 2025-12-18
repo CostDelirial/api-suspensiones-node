52e61051faa9b43d55700a60741f732df1917a49
tactical-testicle <escom.gustavo7@gmail.com>
2025-12-18 13:09:23 -0600

    post general

--- src/routes/ziete.router.ts
+++ src/routes/ziete.router.ts
@@ -5,7 +5,7 @@ import { Router, Request, Response } from "express";
 const zieteRouter = Router()
 const zieteController = new ZieteController()
 
-zieteRouter.get('/general', AuthMiddleware.autetication, zieteController.getGeneral.bind(zieteController))
+zieteRouter.post('/general', AuthMiddleware.autetication, zieteController.getGeneral.bind(zieteController))
 zieteRouter.get('/particular/:uuid_ducto/:fecha_ini/:fecha_fin', AuthMiddleware.autetication, zieteController.getParticular.bind(zieteController))
 
 export default zieteRouter;
