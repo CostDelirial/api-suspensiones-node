3d3c5cf963455e6445f7da827c9a207f3821c679
tactical-testicle <escom.gustavo7@gmail.com>
2025-12-23 12:54:00 -0600

    CREATE DUCTO CON CONTROTABLERO

--- src/daos/catDucto.dao.ts
+++ src/daos/catDucto.dao.ts
@@ -1,4 +1,4 @@
-import { pool } from '../../config/db'; 
+import { pool } from '../../config/db';
 import ICatDucto from '../interfaces/catDucto.interface';
 
 export class CatDuctoDAO {
@@ -17,7 +17,7 @@ export class CatDuctoDAO {
     return result.rows[0] || null;
   }
 
-  static async create(ducto: ICatDucto): Promise<ICatDucto> {
+  /*static async create(ducto: ICatDucto): Promise<ICatDucto> {
     const query = `
       INSERT INTO cat_ducto (nombre, usuario_creacion, status, fecha_creacion)
       VALUES ($1, $2, $3, $4)
@@ -27,7 +27,89 @@ export class CatDuctoDAO {
     const result = await pool.query(query, values);
     console.log("result: ", result)
     return result.rows[0];
+  }*/
+
+  static async create(ducto: ICatDucto): Promise<ICatDucto> {
+    const client = await pool.connect();
+
+    try {
+      await client.query('BEGIN');
+
+      // Crear ducto
+      const insertDuctoQuery = `
+      INSERT INTO cat_ducto (
+        nombre,
+        usuario_creacion,
+        status,
+        fecha_creacion
+      )
+      VALUES ($1, $2, $3, $4)
+      RETURNING *
+    `;
+
+      const ductoValues = [
+        ducto.nombre,
+        ducto.usuarioCreacion,
+        ducto.estatus ?? true,
+        new Date()
+      ];
+
+      const ductoResult = await client.query(insertDuctoQuery, ductoValues);
+      const newDucto = ductoResult.rows[0];
+
+      // Obtener uuid del motivo "CARGA INICIAL"
+      const motivoResult = await client.query(`
+      SELECT uuid
+      FROM cat_motivo
+      WHERE nombre = 'CARGA INICIAL'
+      LIMIT 1
+    `);
+
+      if (motivoResult.rowCount === 0) {
+        throw new Error('No existe el motivo CARGA INICIAL');
+      }
+
+      const uuidMotivo = motivoResult.rows[0].uuid;
+
+      // Insertar registro inicial en tableroControl
+      await client.query(
+        `
+      INSERT INTO tableroControl (
+        uuid_ducto,
+        uuid_motivo,
+        fecha_usuario,
+        status,
+        fecha_creacion,
+        usuario_creacion
+      )
+      VALUES (
+        $1,
+        $2,
+        NOW(),
+        true,
+        NOW(),
+        $3
+      )
+      `,
+        [
+          newDucto.uuid,
+          uuidMotivo,
+          ducto.usuarioCreacion
+        ]
+      );
+
+      await client.query('COMMIT');
+
+      return newDucto;
+
+    } catch (error) {
+      await client.query('ROLLBACK');
+      throw error;
+    } finally {
+      client.release();
+    }
   }
+
 
   static async update(ducto: ICatDucto): Promise<ICatDucto> {
     const query = `
